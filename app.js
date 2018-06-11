904b861696fe769decae8b1e9c9d47ef889dbffc
Yandy Gonzalez <yandygonzalez@gmail.com>
2018-06-11 19:45:15 -0400

    Add query params

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.get('/', function (req, res) {
-  console.log(req);
-  res.send('Home');
+  res.render('index');
 });
 
 app.get('/users/:username', function (req, res) {
@@ -22,6 +21,12 @@ app.get('/books/:bookId', function (req, res) {
 
 app.get('/users/:username/books/:bookId', function (req, res) {
   res.send(req.params)
+});
+
+// Query String = Query Params
+
+app.get('/search', function (req, res) {
+  res.send(req.query)
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
